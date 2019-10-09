766874113900863d9922d7caf24766b48da2dd41
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-10-09 14:44:01 +0900

    [FIXED] aws_clientcredential.h and aws_clientcredential_keys.h
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- demos/include/aws_clientcredential.h
+++ demos/include/aws_clientcredential.h
@@ -31,14 +31,14 @@
  *
  * @todo Set this to the fully-qualified DNS name of your MQTT broker.
  */
-#define clientcredentialMQTT_BROKER_ENDPOINT         "a1u79xipzk3bb1-ats.iot.ap-northeast-1.amazonaws.com"
+#define clientcredentialMQTT_BROKER_ENDPOINT         ""
 
 /*
  * @brief Host name.
  *
  * @todo Set this to the unique name of your IoT Thing.
  */
-#define clientcredentialIOT_THING_NAME               "arn:aws:iot:ap-northeast-1:529241440556:thing/rx65n-gr-rose"
+#define clientcredentialIOT_THING_NAME               ""
 
 /*
  * @brief Port number the MQTT broker is using.
@@ -55,13 +55,13 @@
  *
  * @todo If you are using Wi-Fi, set this to your network name.
  */
-#define clientcredentialWIFI_SSID                    "aterm-1d925a"
+#define clientcredentialWIFI_SSID                    ""
 
 /*
  * @brief Password needed to join Wi-Fi network.
  * @todo If you are using WPA, set this to your network password.
  */
-#define clientcredentialWIFI_PASSWORD                "4b6f75227b29b"
+#define clientcredentialWIFI_PASSWORD                ""
 
 /*
  * @brief Wi-Fi network security type.
