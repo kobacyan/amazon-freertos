3a8649988e11c9c8bbaa87b2af74e5776a15f0d4
vannam.dinh.xt <vannam.dinh.xt@renesas.com>
2019-11-26 10:23:21 +0900

    [UPDATED] Pin settings with new FIT version
    Signed-off-by: vannam.dinh.xt <vannam.dinh.xt@renesas.com>

--- vendors/renesas/boards/rx65n-rsk/aws_demos/src/smc_gen/general/r_cg_macrodriver.h
+++ vendors/renesas/boards/rx65n-rsk/aws_demos/src/smc_gen/general/r_cg_macrodriver.h
@@ -19,10 +19,10 @@
 
 /***********************************************************************************************************************
 * File Name    : r_cg_macrodriver.h
-* Version      : 1.2.5
+* Version      : 1.2.6
 * Device(s)    : R5F565NEDxFC
 * Description  : Macro header file for code generation.
-* Creation Date: 2019-10-09
+* Creation Date: 2019-11-26
 ***********************************************************************************************************************/
 
 #ifndef MACRODRIVER_H
